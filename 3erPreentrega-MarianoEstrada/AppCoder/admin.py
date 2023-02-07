fa70e24835fca5d544e090688efd1680460718db
marianojoey <124524515+marianojoey@users.noreply.github.com>
2023-02-07 20:48:49 -0300

    Add files via upload

---
+++ 3erPreentrega-MarianoEstrada/AppCoder/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from AppCoder.models import *
+
+admin.site.register(Curso)
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+
+
+# Register your models here.
