72085379fec50b354d5744b0d4fd1995e7e8e693
marianojoey <124524515+marianojoey@users.noreply.github.com>
2023-02-09 19:57:20 -0300

    Delete 3erPreentrega-MarianoEstrada directory

--- 3erPreentrega-MarianoEstrada/AppCoder/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from AppCoder.models import *
-
-admin.site.register(Curso)
-admin.site.register(Profesor)
-admin.site.register(Estudiante)
-
-
-# Register your models here.
